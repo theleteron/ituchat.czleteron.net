1dccacf4dba055d5a3f7fde8cb07110358ae92fc
Roman Janiczek <roman@janiczek.cz>
2020-10-17 21:47:53 +0200

    API and other improvements

--- index.php
+++ index.php
@@ -72,8 +72,10 @@
   </head>
 
   <body>
+    <!-- chatArea (all the msgs will be displayed here -->
     <div id="chatArea" style="height: 80%; overflow:auto;"></div> 
 
+    <!-- form for sending new msgs -->
     <div class="submitForm">
       <form onsubmit="return uploadData()">
         <input class="inputBox" type="text" id="newMessageString" placeholder="Your message here ..." minlength="1" maxlength="1024" required>
@@ -81,24 +83,93 @@
       </form>
     </div>
 
+    <!-- status bar (basic debug output) -->
     <div id="status"></div> 
   </body>
 
   <script type="text/javascript">
+    // Help
+    help();
+    function help() {
+      alert("You will be asked for address of API, if you don't remember this you can just continue and default API will be selected.\n" +
+            "Afterwards you will be asked to select your username (max 8 characters, depending on API).\n" +
+            "You can use Alt + S to switch between default APIs.");
+    }
+    // List of default APIs servers 
+    let defaultAPI = ["https://ituchat.czleteron.net/api.php", "http://pckiss.fit.vutbr.cz/itu/api.php", "http://www.stud.fit.vutbr.cz/~xmlich02/itu-ajax/api.php"];
+    var currentAPI = 0;
+    var totalAPI = 4;
     // Address of API server
-    var apiAddress = "https://ituchat.czleteron.net/api.php";
+    var apiAddress = prompt("Enter API address", "");
+    if (apiAddress == null || apiAddress == "") {
+      apiAddress = defaultAPI[0];
+      totalAPI = 3;
+    } else {
+      defaultAPI.push(apiAddress);
+    }
     // Other variables
+    var debug = false;  // Allow debug output to console
     var lastMsgId = 0;
+    // Username setup (doesn't work on school API)
     var user = prompt("Please enter your name", "xlogin00");
-
+    // -- default user setup if not selected
     if (user == null || user == "") {
       user = "xlogin00";
+    }
+
+    // Switch API
+    document.addEventListener('keydown', function(event) {
+      // === DEBUG OUTPUT --- KEY PRESS DETECT ===
+      if (debug) {
+        console.log('%c [DEBUG] ', 'background: #222; color: #0000FF', 'Key Press Detected');
+      }
+      // ====
+      if (event.altKey && event.key === 's') {
+        switchAPI();
+      }
+    })
+    function switchAPI() {
+      // === DEBUG OUTPUT --- CURRENTLY RUNNING FUNC ===
+      if (debug) {
+        console.log('%c [DEBUG] ', 'background: #222; color: #0000FF', 'Currently running function switchAPI()');
+      }
+      // ====
+      // Currently running function - status bar update
+      document.getElementById("status").innerHTML = "switchAPI()";
+      var date = new Date();
+
+      // Move in the list of APIs
+      currentAPI++;
+      if (currentAPI > totalAPI) {
+        currentAPI = 0; // Got too far, go back to start
+      }
+      // Change API
+      apiAddress = defaultAPI[currentAPI];
+      lastMsgId = 0;  // Different chat == different IDs
+      document.getElementById('chatArea').innerHTML += '<span title="API change to ' + apiAddress + '"><span style="color: #949494;">['+ (date.getHours()<10?'0':'') + date.getHours() + ':' 
+                                                    + (date.getMinutes()<10?'0':'') + date.getMinutes() + ']</span> <b style="color: #CD5C5C">CHAT NOW CONNECTED TO</b><i style="color: #00BFFF"> ' + apiAddress + '</i><br>';
+    }
+    function addAndSwitchAPI(server) {
+      var date = new Date();
+      defaultAPI.push(server);
+      totalAPI++;
+      currentAPI = totalAPI - 1;
+      apiAddress = server;
+      lastMsgId = 0;
+      document.getElementById('chatArea').innerHTML += '<span title="API change to ' + apiAddress + '"><span style="color: #949494;">['+ (date.getHours()<10?'0':'') + date.getHours() + ':' 
+                                                    + (date.getMinutes()<10?'0':'') + date.getMinutes() + ']</span> <b style="color: #CD5C5C">CHAT NOW CONNECTED TO</b><i style="color: #00BFFF"> ' + apiAddress + '</i><br>';
     }
 
     /***
       * XMLHttpRequest object constructor (for compatibility with various browsers)
       */
     function createXmlHttpRequestObject() {
+      // === DEBUG OUTPUT --- CURRENTLY RUNNING FUNC ===
+      if (debug) {
+        console.log('%c [DEBUG] ', 'background: #222; color: #0000FF', 'Currently running function createXmlHttpRequestObject()');
+      }
+      // ====
+
       var xmlhttp;
       try {
         xmlHttp = new XMLHttpRequest(); //should work on all browsers except IE6 or older
@@ -117,63 +188,106 @@
     }
 
     function uploadData() {
+      // === DEBUG OUTPUT --- CURRENTLY RUNNING FUNC ===
+      if (debug) {
+        console.log('%c [DEBUG] ', 'background: #222; color: #0000FF', 'Currently running function uploadData()');
+      }
+      // ====
+      // Currently running function - status bar update
       document.getElementById("status").innerHTML = "uploadData()";
 
       try {
-        var xmlHttp = createXmlHttpRequestObject(); //stores XMLHttpRequestObject
+        var xmlHttp = createXmlHttpRequestObject();                                                     // Stores XMLHttpRequestObject
 
-        var params = "data=" + document.getElementById('newMessageString').value + "&user=" + user;
-        xmlHttp.open("POST", apiAddress, true);
+        var params = "data=" + document.getElementById('newMessageString').value + "&user=" + user;     // Data string to be sent
+        xmlHttp.open("POST", apiAddress, true);                                                         // Open HTTP REQ 'POST' for API ADDRESS
         xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
-        xmlHttp.onreadystatechange = downloadData;
+        xmlHttp.onreadystatechange = downloadData;                                                      // Function to run after sending the data
         xmlHttp.send(params);
 
         document.getElementById('newMessageString').value = "";
 
       } catch (e) {
-        alert(e.toString());
+        alert(e.toString());  // Alert box error
       }
 
       return false; // to avoid default form submit behavior 
     }
 
     function downloadData() {
+      // === DEBUG OUTPUT --- CURRENTLY RUNNING FUNC ===
+      if (debug) {
+        console.log('%c [DEBUG] ', 'background: #222; color: #0000FF', 'Currently running function downloadData()');
+      }
+      // ====
+      // Currently running function - status bar update
       document.getElementById("status").innerHTML = "downloadData()";
 
-      var xmlHttp = createXmlHttpRequestObject();
-      xmlHttp.open("GET", apiAddress, true);
-      xmlHttp.onreadystatechange = receiveData;
+      var xmlHttp = createXmlHttpRequestObject();   // Stores XMLHttpRequestObject
+
+      xmlHttp.open("GET", apiAddress, true);        // Open HTTP REQ 'GET' for API ADDRESS
+      xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;");
+      xmlHttp.onreadystatechange = receiveData;     // Function to run on received data
       xmlHttp.send(null);
 
     }
 
     function receiveData() {
+      // === DEBUG OUTPUT --- CURRENTLY RUNNING FUNC ===
+      if (debug) {
+        console.log('%c [DEBUG] ', 'background: #222; color: #0000FF', 'Currently running function receiveData()');
+      }
+      // ====
+      // Currently running function - status bar update
       document.getElementById("status").innerHTML = "receiveData()";
-      var newMsg = false;
+      var newMsg = false; // Local function variable to mark that new msgs were received
 
       if ((xmlHttp.readyState==4) && (xmlHttp.status==200)) { //process is completed and http status is OK
-        var pole = JSON.parse(xmlHttp.responseText);
-        for ( var i in pole ) {
-          if (lastMsgId < pole[i].id) {
-            newMsg = true;
-            let date = new Date(pole[i].dts);
-            document.getElementById('chatArea').innerHTML += '<span title="' + pole[i].id + '" id="' + pole[i].id + '"><span style="color: #949494;">['+ date.getHours() + ':' + date.getMinutes() +
-                                                            ']</span> <b>' + pole[i].login + ':</b> ' + pole[i].cnt + '</span><br>';
-            document.getElementById("status").innerHTML = ' ID of the last msg: ' + pole[i].id + ' from ' + pole[i].login;
-            console.log('%c [NEW MSG] ', 'background: #222; color: #FFA500', pole[i].id);
-            lastMsgId = pole[i].id;
+        // Parse JSON and get MSGs
+        var msgArray = JSON.parse(xmlHttp.responseText);
+        // === DEBUG OUTPUT --- PARSED ARRAY ===
+        if (debug) {
+              console.log('%c [DEBUG] ', 'background: #222; color: #0000FF', msgArray);
+            }
+        // ====
+        for ( var i in msgArray ) {
+          // Check if msg is already in chatArea
+          if (lastMsgId < Number(msgArray[i].id)) {
+            // === DEBUG OUTPUT --- CURRENTLY PARSIN MSG ===
+            if (debug) {
+              console.log('%c [DEBUG] ', 'background: #222; color: #0000FF', msgArray[i]);
+            }
+            // ====
 
+            newMsg = true;                        // Local function variable to mark that new msgs were received
+            let date = new Date(msgArray[i].dts); // Get msg timestamp for future use
+
+            // Add MSG to chatArea and status bar
+            document.getElementById('chatArea').innerHTML += '<span title="' + msgArray[i].id + '" id="' + msgArray[i].id + '"><span style="color: #949494;">['+ (date.getHours()<10?'0':'') + date.getHours() + ':' 
+                                                          + (date.getMinutes()<10?'0':'') + date.getMinutes() + ']</span> <b>' + msgArray[i].login + ':</b> ' + msgArray[i].cnt + '</span><br>';
+            document.getElementById("status").innerHTML = ' ID of the last msg: ' + msgArray[i].id + ' from ' + msgArray[i].login;
+
+            // === DEBUG OUTPUT --- LOG ID OF EVERY NEW MSG TO CONSOLE ===
+            console.log('%c [NEW MSG] ', 'background: #222; color: #FFA500', msgArray[i].id);
+            // ===
+            // Remember last msg
+            lastMsgId = msgArray[i].id;
+
+            // Scroll to the newest msg
             var element = document.getElementById(lastMsgId);
             element.scrollIntoView({ behavior: 'smooth' });
           }
         }
+        // === DEBUG OUTPUT --- LOG THAT NO NEW MSGs WERE RECEIVED ===
         if (!newMsg) {
           console.log('%c [NO NEW MSG]', 'background: #222; color: #008000');
         }
-        document.getElementById("status").innerHTML = ' ID of the last msg: ' + lastMsgId + ' from ' + pole[i].login;
+        // ===
+        // Update status bar
+        document.getElementById("status").innerHTML = ' ID of the last msg: ' + lastMsgId + ' from ' + msgArray[i].login;
       } 
     }
-    downloadData();
-    setInterval(downloadData, 1000);
+    downloadData();                    // Initial download
+    setInterval(downloadData, 1000);   // Download newest msgs every 1s (1000ms)
   </script>
 </html>
